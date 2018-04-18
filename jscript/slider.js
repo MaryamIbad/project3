dd50070784dfeb6d023111632deaac042e723810
MaryamIbad <35270494+MaryamIbad@users.noreply.github.com>
2018-04-18 17:08:06 -0500

    Update and rename script.js to slider.js

--- jscript/slider.js
+++ jscript/slider.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.slider').bxSlider({
       mode: 'fade',
       captions: true,
-      slideWidth: 800
+      slideWidth: 800,
+        responsive: true
     });
   });
